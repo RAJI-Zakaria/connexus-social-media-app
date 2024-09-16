0c4911100ffe54b7dc228ea76e3a9a030882c206
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-09-16 11:17:42 +0200

    customizing the sign-in and sign-up pages

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+const Page = () => {
+  return (
+    <div className="h-[calc(100vh-96px)] flex items-center justify-center">
+      <SignUp />
+    </div>
+  );
+};
+
+export default Page;
